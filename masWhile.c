60158bd049f445d37e8b48f022aaf7b6a2808b72
pamelaitzel <135767172+pamelaitzel@users.noreply.github.com>
2024-05-01 18:53:33 -0600

    Create masWhile.c

---
+++ masWhile.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+    int i = 1;
+    while( i<= 1000){
+        if(i % 2 ==0 && i %7==0){
+            printf("%d\n" , i);
+         }
+         i++;
+
+    }
+    
+    
+    return 0;
+}
